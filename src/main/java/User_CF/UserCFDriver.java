da3a71b15880c57289728ae2afb4dfc5cc6a0f83
guoshen <guo.shen@husky.neu.edu>
2020-04-19 07:50:22 -0700

    add benchmark function in both drivers

--- src/main/java/User_CF/UserCFDriver.java
+++ src/main/java/User_CF/UserCFDriver.java
@@ -7,6 +7,7 @@ public class UserCFDriver {
 		UserCFNormalize normalize = new UserCFNormalize();
 		UserCFMultiplication multiplication = new UserCFMultiplication();
 		UserCFSum sum = new UserCFSum();
+		UserCFBenchmarker benchmarker = new UserCFBenchmarker();
 
 		String rawInput = args[0];
 		String userMovieListOutputDir = args[1];
@@ -14,17 +15,20 @@ public class UserCFDriver {
 		String normalizeDir = args[3];
 		String multiplicationDir = args[4];
 		String sumDir = args[5];
+		String benchmarkDir = args[6];
 		String[] path1 = {rawInput, userMovieListOutputDir};
 		String[] path2 = {userMovieListOutputDir, coOccurrenceMatrixDir};
 		String[] path3 = {coOccurrenceMatrixDir, normalizeDir};
 		String[] path4 = {normalizeDir, rawInput, multiplicationDir};
 		String[] path5 = {multiplicationDir, sumDir};
+		String[] path6 = {rawInput, sumDir, benchmarkDir};
 
 		dataDividerByUser.main(path1);
 		coOccurrenceMatrixGenerator.main(path2);
 		normalize.main(path3);
 		multiplication.main(path4);
 		sum.main(path5);
+		benchmarker.main(path6);
 
 	}
 
